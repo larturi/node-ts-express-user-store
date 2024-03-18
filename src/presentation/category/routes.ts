ca7aefee257a3068ba773a5b904b449bbe0e6225
Leandro Arturi <lea.arturi@gmail.com>
2024-03-18 20:45:36 -0300

    Create category with JWT

--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
 import { CategoryController } from './controller'
+import { AuthMiddleware } from '../middlewares/auth.middleware'
+import { CategoryService } from '../services/category.service'
 
 export class CategoryRoutes {
   static get routes(): Router {
     const router = Router()
-    const controller = new CategoryController()
+    const categoryService = new CategoryService()
+    const controller = new CategoryController(categoryService)
 
     router.get('/', controller.getCategories)
-    router.post('/', controller.createCategory)
+    router.post('/', [AuthMiddleware.validateJWT], controller.createCategory)
 
     return router
   }
